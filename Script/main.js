c10d8f26d634d93f70017837f1ac5ac3f330dcd0
santiago.rosa <santi.rosa@hotmail.com>
2022-03-01 23:50:42 -0300

    se corrige el bucle

--- Script/main.js
+++ Script/main.js
@@ -1,5 +1,5 @@
 const numeroParoImpar = [7,2 ,9 ,98 ,3 ,56 ,23 ,47 ,22 ,26 ,38 ,100 ,91 ,66 ,85];
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < numeroParoImpar.length; i++) {
     const resultado = numeroParoImpar[i]%2;
     if (resultado === 0) {
         console.log(`este numero es par ${numeroParoImpar[i]}`);}
